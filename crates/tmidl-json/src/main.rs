2b88a579407f2efb68dde9e13c96e88ecc028ca4
Layl Bongers <2385329-layl@users.noreply.gitlab.com>
2021-09-25 03:33:24 +0200

    Add json output

--- crates/tmidl-json/src/main.rs
+++ crates/tmidl-json/src/main.rs
@@ -3,6 +3,8 @@ use std::{
     os::raw::c_char,
 };
 
+use serde::Serialize;
+
 use tmidl_sys::{parse_tmidl, Callbacks};
 
 fn main() {
@@ -10,7 +12,7 @@ fn main() {
     let input = std::fs::read_to_string(input_path).unwrap();
     let input = CString::new(input).unwrap();
 
-    let mut user_context = UserContext::default();
+    let mut descriptor = ApiFile::default();
     let callbacks = Callbacks {
         on_item_opaque,
         on_item_interface,
@@ -20,7 +22,7 @@ fn main() {
         parse_tmidl(
             input.as_ptr(),
             &callbacks,
-            &mut user_context as *mut _ as *mut _,
+            &mut descriptor as *mut _ as *mut _,
         )
     };
 
@@ -29,35 +31,30 @@ fn main() {
         return;
     }
 
-    for opaque in user_context.opaques {
-        println!("Opaque: {}", opaque);
-    }
-
-    for interface in user_context.interfaces {
-        println!("Interface: {}", interface);
-    }
+    let json = serde_json::to_string_pretty(&descriptor).unwrap();
+    println!("{}", json);
 }
 
-#[derive(Default)]
-struct UserContext {
+#[derive(Default, Serialize)]
+struct ApiFile {
     opaques: Vec<String>,
     interfaces: Vec<String>,
 }
 
 unsafe extern "C" fn on_item_opaque(name: *const c_char, user_context: *mut c_void) {
-    let user_context = &mut *(user_context as *mut UserContext);
+    let api_file = &mut *(user_context as *mut ApiFile);
     let name = CStr::from_ptr(name);
 
-    user_context
+    api_file
         .opaques
         .push(name.to_string_lossy().to_string());
 }
 
 unsafe extern "C" fn on_item_interface(name: *const c_char, user_context: *mut c_void) {
-    let user_context = &mut *(user_context as *mut UserContext);
+    let api_file = &mut *(user_context as *mut ApiFile);
     let name = CStr::from_ptr(name);
 
-    user_context
+    api_file
         .interfaces
         .push(name.to_string_lossy().to_string());
 }
