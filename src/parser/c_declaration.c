75a39bf683188d25715104a3097f6abe5c85d205
Layl Bongers <2385329-layl@users.noreply.gitlab.com>
2021-09-30 04:27:05 +0200

    Start parsing fields again

--- src/parser/c_declaration.c
+++ src/parser/c_declaration.c
@@ -2,6 +2,51 @@
 #include "mpc_utils.h"
 #include "doc_comment.h"
 
+static mpc_val_t *fold_field(int n, mpc_val_t **xs)
+{
+    free(xs[0]);
+    free(xs[1]);
+    free(xs[3]);
+    free(xs[4]);
+
+    return xs[2];
+}
+
+static mpc_parser_t *field_parser()
+{
+    mpc_parser_t *before = mpc_and(2, mpcf_all_free, mpc_not(mpc_char('('), free), mpc_any(), free);
+    mpc_parser_t *after = mpc_and(2, mpcf_all_free, mpc_not(mpc_char(';'), free), mpc_any(), free);
+
+    mpc_parser_t *name = mpc_and(
+        2, mpcf_snd_free,
+        mpc_char('*'), identifier());
+
+    return mpc_and(
+        5, fold_field,
+        doc_comment_parser(),
+        mpc_many(mpcf_all_free, before),
+        mpc_tok_parens(name, free),
+        mpc_many(mpcf_all_free, after),
+        mpc_tok(mpc_char(';')),
+        free, free, free);
+}
+
+static mpc_val_t *fold_body(int n, mpc_val_t **xs)
+{
+    for (int i = 0; i < n; i++) {
+        printf("Field: %s\n", xs[i]);
+        free(xs[i]);
+    }
+
+    return NULL;
+}
+
+static mpc_parser_t *body_parser()
+{
+    mpc_parser_t *fields = mpc_many(fold_body, field_parser());
+    return mpc_tok_brackets(fields, free);
+}
+
 static mpc_val_t *fold_type_specifier_struct(int n, mpc_val_t **xs)
 {
     mpc_state_t *state = xs[1];
@@ -21,18 +66,12 @@ static mpc_parser_t *type_specifier_struct_parser()
 {
     mpc_parser_t *struct_tok = mpc_tok(mpc_string("struct"));
 
-    mpc_parser_t *body_c = mpc_and(2, mpcf_all_free, mpc_not(mpc_char('}'), free), mpc_any(), free);
-    mpc_parser_t *body = mpc_and(
-        3, mpcf_all_free,
-        mpc_tok(mpc_char('{')), mpc_many(mpcf_all_free, body_c), mpc_tok(mpc_char('}')),
-        free, free);
-
     return mpc_and(
         4, fold_type_specifier_struct,
         struct_tok,
         // Name
         mpc_state(), mpc_tok(identifier()),
-        mpc_maybe(body),
+        mpc_maybe(body_parser()),
         free, free, free);
 }
 
